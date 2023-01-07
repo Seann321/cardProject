91475a299d8cb6a1e5f2a105d6b860aea6d072a8
Sean <officialseann@gmail.com>
2023-01-07 18:41:59 -0500

    Change to port

--- main.py
+++ main.py
@@ -177,4 +177,4 @@ def blank():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, host='0.0.0.0', port=os.getenv("PORT", default=5000))
